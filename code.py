74d559466c7a2725ea5ecf546685daef6b6a3988
Xuan Hai <dxhai311@gmail.com>
2026-05-17 11:09:04 +0700

    Add function so clean code

--- code.py
+++ code.py
@@ -7,6 +7,53 @@ class Student:
 	def __str__(self):
 		return f"{self.ten} {self.score}"
 
+def show_students(students):
+	for name, score in students.items():
+		sv = Student(name, score)
+		print(sv)
+
+def add_student(students):
+	name = input('Enter name :').title()
+	if name not in students:
+		try:
+			score = float(input('Enter score :'))
+			if 0 <= score <= 10:
+					students[name] = score
+			else:
+					print('Score Invalid')
+		except ValueError:
+				print('Invalid score')	
+	else:
+		print('Student already exists')
+
+def delete_student(students):
+	name_del = input('Enter name to delete :').title()
+	if name_del in students:
+		del students[name_del]
+		print('Delete succesfully')
+	else:
+		print('Invalid name')
+
+def update_student(students):
+	name_update = input('Enter student name :').title()
+	if name_update not in students:
+		print('Invalid name')
+	try:
+		score_update = float(input('Enter new score :'))
+		if 0 <= score_update <= 10:	
+			students[name_update] = score_update
+			print('Update succesfully')
+		else:
+			print('Invalid score')
+	except ValueError:
+		print('Invalid score update')
+
+def search_student(students):
+	name_search = input('Enter name search :').title()
+	if name_search in students:
+		print(name_search, students[name_search], sep = ' - ')
+	else:
+		print('Student not found')
 
 if __name__ == '__main__':
 	students = {
@@ -31,22 +78,9 @@ if __name__ == '__main__':
 			print('Invalid input')
 			continue
 		if select == 1:
-			for name, score in students.items():
-				sv = Student(name, score)
-				print(sv)
+			show_students(students)
 		elif select == 2:
-			name = input('Enter name :').title()
-			if name not in students:
-				try:
-					score = float(input('Enter score :'))
-					if 0 <= score <= 10:
-						students[name] = score
-					else:
-						print('Score Invalid')
-				except ValueError:
-					print('Invalid score')	
-			else:
-				print('Student already exists')
+			add_student(students)
 		elif select == 3:
 			res = sum(score for name, score in students.items()) / len(students)
 			print(f"Average score : {res:.2f}")
@@ -54,33 +88,11 @@ if __name__ == '__main__':
 			top_name, top_score = max(students.items(), key = lambda x : x[1])
 			print(f"Top student : {top_name} - {top_score}")
 		elif select == 5:
-			name_del = input('Enter name to delete :').title()
-			if name_del in students:
-				del students[name_del]
-				print('Delete succesfully')
-				continue
-			else:
-				print('Invalid name')
+			delete_student(students)
 		elif select == 6:
-			name_update = input('Enter student name :').title()
-			if name_update not in students:
-				print('Invalid name')
-			try:
-				score_update = float(input('Enter new score :'))
-				if 0 <= score_update <= 10:	
-					students[name_update] = score_update
-					print('Update succesfully')
-				else:
-					print('Invalid score')
-			except ValueError:
-				print('Invalid score update')
-				continue
+			update_student(students)
 		elif select == 7:
-			name_search = input('Enter name search :').title()
-			if name_search in students:
-				print(name_search, students[name_search], sep = ' - ')
-			else:
-				print('Student not found')
+			search_student(students)
 		elif select == 8:
 			check = False
 		else:
